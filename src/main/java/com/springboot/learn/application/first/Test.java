1677213a12121c6d3acabd6a73b729a0dbaf741c
listen <1083116757@qq.com>
2017-10-31 21:55:15 +0800

    初始化数据

---
+++ src/main/java/com/springboot/learn/application/first/Test.java
@@ -0,0 +1,14 @@
+package com.springboot.learn.application.first;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "test";
+    }
+}
